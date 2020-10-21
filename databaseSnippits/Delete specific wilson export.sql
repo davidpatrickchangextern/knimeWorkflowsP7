65d0bdf00badcb28c54ccd059068520f187735f8
davetesting for own company <david.chang@abis-cloud.com>
2020-10-21 21:58:31 +0200

    Added new reference value

---
+++ databaseSnippits/Delete specific wilson export.sql
@@ -0,0 +1,4 @@
+SET SQL_SAFE_UPDATES=0;
+Delete FROM p7_stories.wilsonmaster where Date_To = '2020-10-18';
+SET SQL_SAFE_UPDATES=1;
+Select * FROM p7_stories.wilsonmaster where Date_To = '2020-10-18';
